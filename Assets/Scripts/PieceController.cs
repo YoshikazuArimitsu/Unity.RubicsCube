232ea31dae071d84529174c3ce6dc742d49c1543
有光喜一 <arimitu@ouk.jp>
2017-05-26 19:50:08 +0900

    なぜかマウスに反応しない

--- Assets/Scripts/PieceController.cs
+++ Assets/Scripts/PieceController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PieceController : MonoBehaviour {
+    private Transform BaseCube_;
+    private RubicsCubeController RubicsCubeController_;
 
 	// Use this for initialization
 	void Start () {
-		
+        BaseCube_ = transform.parent;
+        RubicsCubeController_ = BaseCube_.parent.GetComponent<RubicsCubeController>();
+
+        if(RubicsCubeController_ == null) {
+            Debug.Log("Error?");
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void OnMouseDown() {
+        Debug.LogFormat("OnMouseDown on {0}", transform.name);
+    }
 }
